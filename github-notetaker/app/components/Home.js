59318e8008277e4008ec7cb26f0b0d8ef2a3830a
beapeter <jjwonmin@naver.com>
2016-01-21 02:00:03 +0900

    Basic Routing with React Router

--- github-notetaker/app/components/Home.js
+++ github-notetaker/app/components/Home.js
@@ -1,1 +1,14 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
 
+var Home = React.createClass({
+    render: function() {
+        return (
+            <h2 className="text-center">
+                Search by Github Username Above
+            </h2>
+        )
+    }
+})
+
+module.exports = Home;
