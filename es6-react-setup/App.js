fe58e2f744ade7e7f725641533866ee898314db0
beapeter <jjwonmin@naver.com>
2016-01-24 04:46:21 +0900

    React Fundamemtals: Introduction to Properties

--- es6-react-setup/App.js
+++ es6-react-setup/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
     render(){
+        let txt = this.props.txt;
+        let cat = this.props.cat;
         return (
             <div>
-                <h1>Hello World</h1>
-                <b>Hey!</b>
+                <h1>{txt}, {cat}</h1>
             </div>
         );
 
@@ -14,4 +16,16 @@ class App extends React.Component {
 
 //const App = () => <h1>Hello Wonmin</h1>
 
-export default App
+App.propTypes = {
+    txt: React.PropTypes.string,
+    cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+    txt: 'this is the default txt'
+}
+
+ReactDOM.render(
+    <App cat={36} />,
+    document.getElementById('app')
+);
