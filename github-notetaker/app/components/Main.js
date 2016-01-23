51f32ee121503f075046736f3a360fe069bc1423
beapeter <jjwonmin@naver.com>
2016-01-24 02:31:51 +0900

    ES6 Refactor: Routing without Mixins

--- github-notetaker/app/components/Main.js
+++ github-notetaker/app/components/Main.js
@@ -1,21 +1,21 @@
-var React = require('react');
-var SearchGithub = require('./SearchGithub');
+import React from 'react';
+import SearchGithub from './SearchGithub';
 
-var Main = React.createClass({
-   render: function() {
-       return (
-           <div className="main-container">
-               <nav className="navbar navbar-default" role="navigation">
-                   <div className="col-sm-7 col-sm-offset-2" style={{marginTop: 15}}>
-                       <SearchGithub />
-                   </div>
-               </nav>
-               <div className="container">
-                   {this.props.children}
-               </div>
-           </div>
-       )
-   }
-});
+class Main extends React.Component {
+    render() {
+        return (
+            <div className="main-container">
+                <nav className="navbar navbar-default" role="navigation">
+                    <div className="col-sm-7 col-sm-offset-2" style={{marginTop: 15}}>
+                        <SearchGithub history={this.props.history} />
+                    </div>
+                </nav>
+                <div className="container">
+                    {this.props.children}
+                </div>
+            </div>
+        )
+    }
+}
 
-module.exports = Main;
+export default Main;
