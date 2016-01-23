43bb5e71a3e0c802e3d946933b1712d51d8ea52e
beapeter <jjwonmin@naver.com>
2016-01-24 04:36:48 +0900

    React Fundamemtals: The Render Method

---
+++ es6-react-setup/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>Hello World</h1>
+                <b>Hey!</b>
+            </div>
+        );
+
+    }
+}
+
+//const App = () => <h1>Hello Wonmin</h1>
+
+export default App
