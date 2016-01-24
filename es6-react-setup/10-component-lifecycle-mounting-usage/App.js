c686b04768d52d6f0bf1c8745bd4b39b79fb4c21
beapeter <jjwonmin@naver.com>
2016-01-24 16:51:02 +0900

    React Fundamentals: Component LifeCycle - Mounting Usage

--- es6-react-setup/10-component-lifecycle-mounting-usage/App.js
+++ es6-react-setup/10-component-lifecycle-mounting-usage/App.js
@@ -2,14 +2,52 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    render() {
-        return <h1>Hello World!</h1>
+    constructor(){
+        super();
+        this.state = {val: 0};
+        this.update = this.update.bind(this);
+    }
+    update(){
+        this.setState({val: this.state.val + 1})
+    }
+    componentWillMount(){
+        this.setState({m: 2})
+    }
+    render(){
+        console.log('rendering!')
+        return (
+            <button onClick={this.update}>
+                {this.state.val * this.state.m}
+            </button>
+        )
+    }
+    componentDidMount(){
+        this.inc = setInterval(this.update, 500)
+    }
+    componentWillUnmount(){
+        clearInterval(this.inc)
     }
 }
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('app')
-);
+class Wrapper extends React.Component {
+    constructor(){
+        super();
+    }
+    mount(){
+        ReactDOM.render(<App />, document.getElementById('a'))
+    }
+    unmount(){
+        ReactDOM.unmountComponentAtNode(document.getElementById('a'))
+    }
+    render(){
+        return (
+            <div>
+                <button onClick={this.mount.bind(this)}>Mount</button>
+                <button onClick={this.unmount.bind(this)}>Unmount</button>
+                <div id="a"></div>
+            </div>
+        )
+    }
+}
 
-export default App
+export default Wrapper
