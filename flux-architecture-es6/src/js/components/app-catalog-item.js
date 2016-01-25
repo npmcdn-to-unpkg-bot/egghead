702c2c4bd96ab9fb46f9e9851bfcad42e881d4b4
beapeter <jjwonmin@naver.com>
2016-01-26 02:25:56 +0900

    Flux Architecture: Higher-Order Components

--- flux-architecture-es6/src/js/components/app-catalog-item.js
+++ flux-architecture-es6/src/js/components/app-catalog-item.js
@@ -8,7 +8,7 @@ export default (props) => {
             <h4>{props.item.title}</h4>
             <img src="http://placehold.it/250X250" width="100%" className="img-responsive" />
             <p>{props.item.summary}</p>
-            <p>${props.item.cost}</p>
+            <p>${props.item.cost} <span className="text-success">{props.item.qty && `(${props.item.qty} in cart)`}</span></p>
             <CartButton handler={AppActions.addItem.bind(null, props.item)} txt="Add To Cart" />
         </div>
     )
