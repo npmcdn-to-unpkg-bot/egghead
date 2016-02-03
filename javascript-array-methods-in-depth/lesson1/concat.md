7885d2933f642e02abcee64740f5ad8c1dd3e11b
beapeter <jjwonmin@naver.com>
2016-02-03 23:52:28 +0900

    first commit

---
+++ javascript-array-methods-in-depth/lesson1/concat.md
@@ -0,0 +1,5 @@
+#구문
+
+```javascript
+var new_array = old_array.concat(value1[, value2[, ...[, valueN]]])
+```
--- javascript-array-methods-in-depth/lesson2/join.js
+++ javascript-array-methods-in-depth/lesson2/join.js
@@ -1,3 +1,8 @@
-/**
- * Created by jeonwonmin on 2016. 2. 3..
- */
+// map() 메소드는 배열 내의 모든 요소 내에서 제공된 함수를 호출하여 그 결과로 새로운 배열을 생성합니다. from mdn
+
+var name = 'shane osbourne';
+
+var upper = name.split(' ') // [shane, osbourne]
+        .map(x => x.charAt(0).toUpperCase() + x.slice(1)).join(' ');  // [Shane, Osbourne]
+
+console.log(upper);
