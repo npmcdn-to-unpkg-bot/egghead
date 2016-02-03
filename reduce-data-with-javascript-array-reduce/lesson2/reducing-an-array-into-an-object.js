cd247368e0931b921f3b53d329a7e6e28356085e
beapeter <jjwonmin@naver.com>
2016-02-04 01:50:23 +0900

    reducing an array into an object

---
+++ reduce-data-with-javascript-array-reduce/lesson2/reducing-an-array-into-an-object.js
@@ -0,0 +1,27 @@
+var votes = [
+    "angular",
+    "angular",
+    "react",
+    "react",
+    "react",
+    "angular",
+    "ember",
+    "react",
+    "vanilla"
+];
+
+var initialValue = {};
+
+var reducer = function(tally, vote) {
+    if (!tally[vote]) {
+        tally[vote] = 1;
+    } else {
+        tally[vote] = tally[vote] + 1;
+    }
+
+    return tally;
+};
+
+var result = votes.reduce(reducer, initialValue);
+
+console.log(result);
