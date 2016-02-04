69ed9972cc544165696023d641511b557cd1adfd
jeon-wonmin <wonmin.jeon@tmon.co.kr>
2016-02-04 22:09:26 +0900

    composing funtions with reduce

---
+++ reduce-data-with-javascript-array-reduce/lesson7/composing-functions-with-reduce.js
@@ -0,0 +1,31 @@
+function increment(input) { return input + 1; }
+function decrement(input) { return input - 1; }
+function double(input) { return input * 2 ;}
+function halve(input) { return input / 2; }
+
+var initial_value = 1;
+//var incremented_value = increment(initial_value);
+//var doubled_value = double(incremented_value);
+//var final_value = decrement(doubled_value);
+
+//function transform(input) {
+//    return ((input + 1) * 2) - 1;
+//}
+
+//var final_value = transform(initial_value);
+
+var pipeline = [
+    increment,
+    increment,
+    increment,
+    double,
+    increment,
+    increment,
+    halve
+];
+
+var final_value = pipeline.reduceRight(function(acc, fn) {
+    return fn(acc);
+}, initial_value);
+
+console.log(final_value);
